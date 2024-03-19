5db14d51e292b0503cbe7fd398f2ea42b59f1f8c
sohail mannan <sohailmannan54@gmail.com>
2024-03-19 06:13:38 +0500

    al

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,29 @@ function App() {
 
   return (
     <div className="timer-container">
+
       <div className="btn-container">
         <button className="btn" onClick={start}>Start</button>
         <button className="btn" onClick={pause}>Pause</button>
         <button className="btn" onClick={reset}>Reset</button>
       </div>
+
       <div className="timer">
         <div className="time-box">
           <p className="time">{hours}</p>
           <p className="title">Hour</p>
         </div>
+
         <div className="time-box">
           <p className="time">{minutes}</p>
           <p className="title">Minute</p>
         </div>
+
         <div className="time-box">
           <p className="time">{seconds}</p>
           <p className="title">Second</p>
         </div>
+        
       </div>
       {status === 'RUNNING' && <p> RUNNING <span className="status blinking"> . . . .</span></p>}
     </div>
