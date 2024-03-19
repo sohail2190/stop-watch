c42e9d0498095b9dde7c6a7f01429e3c32dc714f
sohail mannan <sohailmannan54@gmail.com>
2024-03-19 06:03:38 +0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   const { time, start, pause, reset, status } = useTimer();
 
+  // Calculate hours, minutes, and seconds
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
   return (
     <div className="timer-container">
       <div className="btn-container">
@@ -11,7 +16,20 @@ function App() {
         <button className="btn" onClick={pause}>Pause</button>
         <button className="btn" onClick={reset}>Reset</button>
       </div>
-      <p className="timer">Start Time: {time} sec</p>
+      <div className="timer">
+        <div className="time-box">
+          <p className="time">{hours}</p>
+          <p className="title">Hour</p>
+        </div>
+        <div className="time-box">
+          <p className="time">{minutes}</p>
+          <p className="title">Minute</p>
+        </div>
+        <div className="time-box">
+          <p className="time">{seconds}</p>
+          <p className="title">Second</p>
+        </div>
+      </div>
       {status === 'RUNNING' && <p> RUNNING <span className="status blinking"> . . . .</span></p>}
     </div>
   );
